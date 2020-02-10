17737cb233dae7a4327de9e6a157974202f3d05e
Lewis Munene <munenelewis77@gmail.com>
2020-02-10 22:56:17 +0300

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import './index.scss';
+import './app.scss';
+import { Content } from 'carbon-components-react/lib/components/UIShell';
+import AppHeader from './components/header/AppHeader';
+
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
+      <>
+        <AppHeader />
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
+      </>
     );
   }
 }
